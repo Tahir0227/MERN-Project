be3898354d89796091e50e0c5ea6fa0589c2c2bb
Sarthak Katkar <sarthakkatkar14@gmail.com>
2025-12-23 20:09:09 +0530

    Added student API

---
+++ SERVER with AUTHENTICATION/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+
+const courseRouter = require('./routes/courses')
+const videosRouter = require('./routes/videos')
+const studentDataRouter  = require('./routes/videos')
+const commonRouter = require('./routes/common')
+const {authUser, checkAuthorization} = require('./utils/auth')
+const studentRouter = require('./routes/student')
+
+app.use(express.json())
+app.use(authUser)
+app.use("/courses", courseRouter)
+app.use("/videos", videosRouter)
+app.use("/", studentDataRouter)
+app.use("/",commonRouter)
+app.use("/student",studentRouter)
+
+app.listen(4000, 'localhost', (req, res) => {
+    console.log("Server started at 4000")
+})
+
+//nodemon server.js
---
+++ SERVER with AUTHENTICATION/utils/auth.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+const config = require('./config')
+const result = require('./result')
+
+function authUser(req, res, next){
+    const path = req.url
+    if(path == '/auth/login' || path =='/courses/all-active-courses' || path == '/auth/sign-up')
+        next()
+
+    else{
+        const token = req.headers.token
+        if(!token)
+            res.send(result.createResult('Token is missing'))
+
+        else{
+            try{
+                const payload = jwt.verify(token, config.SECRET)
+                req.headers.email = payload.emial
+                req.headers.role = payload.role
+                next()
+            }
+            catch(e){
+                res.send(result.createResult('Token is invalid'))
+            }
+            
+        }
+    }
+}
+
+function checkAuthorization(req, res, next) {
+    const role = req.headers.role
+    console.log("current user role: ", role);
+
+    if(role == "admin")
+        return next()
+
+    return res.send(result.createResult("Unauthorized Access"))
+}
+
+module.exports = {authUser, checkAuthorization}
