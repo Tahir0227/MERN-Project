f085360c5a05b61d97dff2ab56d54d6c4365dc9d
Sarthak Katkar <sarthakkatkar14@gmail.com>
2025-12-27 18:03:32 +0530

    Updated App.jsx file

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -7,6 +7,9 @@ import Register from "./Pages/Register"
 import Profile from "./Pages/Profile"
 import { createContext, useState } from "react"
 import Courseinfo from "./Pages/Courseinfo"
+import CourseVideos from "./Pages/CourseVideos"
+import MyCourses from './Pages/MyCourses';
+import MySelectedCourse from './Pages/MySelectedCourse';
 
 export const LoginContext = createContext()
 
@@ -23,6 +26,11 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/register" element={<Register />} />
           <Route path="/course-info" element={<Courseinfo />} />
+          <Route path="/course-video" element={<CourseVideos />} />
+          <Route path="/my-selected-courses" element={<MySelectedCourse />} />
+          <Route path="/my-courses" element={<MyCourses />} />
+
+
         </Routes>
       </LoginContext.Provider>
       <ToastContainer />
