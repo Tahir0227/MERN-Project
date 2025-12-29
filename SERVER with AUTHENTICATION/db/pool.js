b8eed3d3e58e2eeaa2c1f7f6f00dce7e39909ce2
Tahir Handimalik <tahirhandimalik@gmail.com>
2025-12-29 12:13:02 +0530

    updated files

--- SERVER with AUTHENTICATION/db/pool.js
+++ SERVER with AUTHENTICATION/db/pool.js
@@ -3,7 +3,7 @@ const sql = require("mysql2")
 const pool = sql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'manager',
+    password: 'manger',
     database: 'sunbeam1'
 })
 
--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -8,21 +8,12 @@ import Profile from "./Pages/Profile"
 import { createContext, useState } from "react"
 import Courseinfo from "./Pages/Courseinfo"
 import MyCourses from "./Pages/MyCourses"
-<<<<<<< HEAD
 import MySelectedCourse from "./Pages/MySelectedCourse"
-=======
-import MyProfile from "./Pages/MyProfile"
-import AdminHome from "./AdminPages/AdminHome"
-import AddNewCourse from "./AdminPages/AddNewCourse"
-import AllCourses from "./AdminPages/AllCourses"
-import UpdateCourse from "./AdminPages/UpdateCourse"
-import CourseVideos from "./Pages/CourseVideos"
->>>>>>> 38d318c874a98170abbf268cc59eed508e2ab987
 
 export const LoginContext = createContext()
 
 function App() {
- 
+
   const [loginStatus, setLoginStatus] = useState(false)
 
   return (
@@ -35,17 +26,7 @@ function App() {
           <Route path="/registerCourse/:id" element={<Register />} />
           <Route path="/course-info/:id" element={<Courseinfo />} />
           <Route path="/my-course" element={<MyCourses />} />
-<<<<<<< HEAD
           <Route path="/my-course-info/:id" element={<MySelectedCourse />} />
-=======
-          <Route path="/my-profile" element={<MyProfile />} />
-          <Route path="/admin-home" element={<AdminHome />} />
-          <Route path="/add-new-course" element={<AddNewCourse />} />
-          <Route path="/all-courses" element={<AllCourses />} />
-          <Route path="/update-course/:id" element={<UpdateCourse />} />
-          <Route path="/courses-videos" element={<CourseVideos />} />
-
->>>>>>> 38d318c874a98170abbf268cc59eed508e2ab987
 
 
         </Routes>
